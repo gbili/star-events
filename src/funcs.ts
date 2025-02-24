8f09b88381e5a58c2fec232af194ed621ccfcd7c
Guillermo Pages <mail@guillermo.at>
2025-02-24 20:56:14 +0100

    fix: remove unused

--- src/funcs.ts
+++
@@ -1,3 +0,0 @@
-export const dateToString = (d: Date) => `${d.toLocaleDateString('fr-CA')} ${d.toLocaleTimeString('fr-CH')}`;
-export const devNull = (..._: any[]): void => undefined;
-export const getTimestamp = (): string => `[${dateToString(new Date())}]`;
